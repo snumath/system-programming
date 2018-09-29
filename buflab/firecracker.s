fff1a676a5e365eb15961c05cc24a95d77ccbc59
DS-Ubuntu <96lives@gmail.com>
2018-09-29 23:24:46 +0900

    dynamite finished

---
+++ buflab/firecracker.s
@@ -0,0 +1,3 @@
+movl $0x0407, 0x0804d120
+pushl $0x8048c81
+ret
---
+++ buflab/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int *argc, char** argv) {
+
+    char arr[40];
+    gets(arr);
+    int a = 1;
+    int b = 2;
+    int c = a + b;
+    return 0;
+}
